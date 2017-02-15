718291797d1a0e272a6161cf135600260c5d83d5
nova1023 <thomas.yu1023@gmail.com>
2017-02-14 21:47:12 -0500

    tried adding js for scoring

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,34 +6,68 @@ var timerInterval;
 var playerClick;
 
 
-window.onload = function() {
-	$("#main-content").hide();
-	$("#score-container").hide();
-};
-
-$("#start-game").click(function() {
-	$("#main-content").show();
-	$("#start-game").hide();
-	timerInterval = setInterval(countDown, 1000);
-	countDown();
-});
+// window.onload = function() {
+// 	$("#main-content").hide();
+// 	$("#score-container").hide();
+// };
 
 
+// $("#start-game").click(function() {
+// 	$("#main-content").show();
+// 	$("#start-game").hide();
+// 	timerInterval = setInterval(countDown, 1000);
+// 	countDown();
+// });
 
 
-function countDown(){
-	timer--;
-	$("#timer-integer").html(timer);
-	if (timer === 0) {
-		stopTimer();
-		$("#main-content").hide();
-		$("#score-container").show();
-		$("#timeUpOrDone").html("Time is up!");
-	}
-}
+// function countDown(){
+// 	timer--;
+// 	$("#timer-integer").html(timer);
+// 	if (timer === 0) {
+// 		stopTimer();
+// 		$("#main-content").hide();
+// 		$("#score-container").show();
+// 		$("#timeUpOrDone").html("Time is up!");
+// 	}
+// }
+
+
+
 
 // do I need this since the div will be hidden anyway?
 function stopTimer(){
 	clearInterval(timerInterval);
 }
+
+
+
+
+// Starting here is where I'm running into a problem
+
+var answerOne = $('#a1').prop('checked');
+
+function questionOne() {
+	if (answerOne = true) {
+		correct++;
+		$("#correct-integer").html(correct);
+	}
+	else {
+		incorrect++;
+		$("#incorrect-integer").html(correct);
+	}
+}
+questionOne();
+
+
+
+// $("input").check(function() {
+// 	if (answerOne = true){
+// 		correct++;
+// 		$("#correct-integer").html(correct);
+// 	}
+// 	else {
+// 		incorrect++;
+// 		$("#incorrect-integer").html(incorrect);
+// 	}
+// });
 
