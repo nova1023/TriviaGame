eb8475bd546bb42a01a553bce6af603a4ab4c49e
nova1023 <thomas.yu1023@gmail.com>
2017-02-16 14:59:47 -0500

    added more code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,15 +3,12 @@ var incorrect = 0;
 var unanswered = 0;
 var timer = 30;
 var timerInterval;
-var playerClick;
-
 
 // window.onload = function() {
 // 	$("#submitButton").hide();
 // 	$("#main-content").hide();
 // 	$("#score-container").hide();
 // };
-
 
 // $("#start-game").on("click", function() {
 // 	$("#main-content").show();
@@ -20,7 +17,6 @@ var playerClick;
 // 	timerInterval = setInterval(countDown, 1000);
 // 	countDown();
 // });
-
 
 // function countDown(){
 // 	timer--;
@@ -31,6 +27,7 @@ var playerClick;
 // 		$("#submitButton").hide();
 // 		$("#score-container").show();
 // 		$("#timeUpOrDone").html("Time is up!");
+// 		notAnswered();
 // 	}
 // }
 
@@ -38,56 +35,110 @@ var playerClick;
 // 	clearInterval(timerInterval);
 // }
 
-
-
-
-// Starting here is where I'm running into a problem
-
-// var answerOne = $('#a1').prop('checked');
-// var wrongOne = $('.wrongClass1').prop('checked');
-
-
- // use .each
+//When you click on Submit, checks if each question was answered
 
 $("#submitButton").on("click", function() {
-	var answerOne = $('#a1').prop('checked');
-	var wrongOne = $('.wrongClass1').prop('checked');
+	$("#submitButton").hide();
+	$("#main-content").hide();
+	$("#score-container").show();
+	$("#timeUpOrDone").html("You beat the timer!");
+	stopTimer();
+
+	var answerOne = $('#answer1').prop('checked');
 
 	if (answerOne === true) {
 		correct++;
 		$("#correct-integer").html(correct);
-
 	}
-	else if (wrongOne === true) {
+	else {
 		incorrect++;
 		$("#incorrect-integer").html(incorrect);
+		}
+
+	// question 2
+	var answerTwo = $('#answer2').prop('checked');
+
+	if (answerTwo === true) {
+		correct++;
+		$("#correct-integer").html(correct);
+	}
+	else {
+		incorrect++;
+		$("#incorrect-integer").html(incorrect);
+		}
+
+	// question 3
+	var answerThree = $('#answer3').prop('checked');
+
+	if (answerThree === true) {
+		correct++;
+		$("#correct-integer").html(correct);
+	}
+	else {
+		incorrect++;
+		$("#incorrect-integer").html(incorrect);
+		}
+
+	//question 4
+	var answerFour = $('#answer4').prop('checked');
+
+	if (answerFour === true) {
+		correct++;
+		$("#correct-integer").html(correct);
+	}
+	else {
+		incorrect++;
+		$("#incorrect-integer").html(incorrect);
+		}
+
+	//question 5
+	var answerFive = $('#answer5').prop('checked');
+
+	if (answerFive === true) {
+		correct++;
+		$("#correct-integer").html(correct);
+	}
+	else {
+		incorrect++;
+		$("#incorrect-integer").html(incorrect);
+		}
+
+});	
+
+// if there were no selections made for each question the count for unanswered goes up
+function notAnswered() {
+	//if nothing selected for question 1
+	var nothingQ1 = $(".class1").prop('checked');
+	if (nothingQ1 === false) {
+		unanswered++;
+		$("#unanswered-integer").html(unanswered);
 	}
 
-});
+	//if nothing selected for question 2
+	var nothingQ2 = $(".class2").prop('checked');
+	if (nothingQ2 === false) {
+		unanswered++;
+		$("#unanswered-integer").html(unanswered);
+	}
 
+	//if nothing selected for question 3
+	var nothingQ3 = $(".class3").prop('checked');
+	if (nothingQ3 === false) {
+		unanswered++;
+		$("#unanswered-integer").html(unanswered);
+	}
 
-// $("#a1").on("click", function() {
-// 		if (answerOne === true) {
-// 			correct++;
-// 			$("#correct-integer").html(correct);
-// 		}
-// 		else {
-// 			incorrect++;
-// 			$("#incorrect-integer").html(incorrect);
-// 		}
-// 	});
+	//if nothing selected for question 4
+	var nothingQ4 = $(".class4").prop('checked');
+	if (nothingQ4 === false) {
+		unanswered++;
+		$("#unanswered-integer").html(unanswered);
+	}
 
-
-
-
-// $("input").check(function() {
-// 	if (answerOne = true){
-// 		correct++;
-// 		$("#correct-integer").html(correct);
-// 	}
-// 	else {
-// 		incorrect++;
-// 		$("#incorrect-integer").html(incorrect);
-// 	}
-// });
-
+	//if nothing selected for question 5
+	var nothingQ5 = $(".class5").prop('checked');
+	if (nothingQ5 === false) {
+		unanswered++;
+		$("#unanswered-integer").html(unanswered);
+	}
+}
